23d93ce2caf86d793257ab81cc0aa01d9bfeadda
julhamarcolan <juliamarcolant@gmail.com>
2024-03-05 18:17:11 -0300

    ADD FILES

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import telepot
+from telepot.loop import MessageLoop
+from nlp import ia
+import env
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    nlp_response = ia(msg['text'])
+    if content_type == 'text':
+        bot.sendMessage(chat_id, nlp_response)
+
+
+#TOKEN = sys.argv[1]  # get token from command-line
+TOKEN = env.token
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
---
+++ nlp.py
@@ -0,0 +1,22 @@
+import google.generativeai as genai
+
+# https://aistudio.google.com/app/apikey
+import env 
+
+def ia(user_msg):
+  
+    genai.configure(api_key= env.GOOGLE_API_KEY)
+    
+    for m in genai.list_models():
+      if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+    
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(user_msg)
+    #print(response.text)
+    msg = response.text
+    
+    # Speech Analysis
+    #print(response.prompt_feedback)
+    return msg
+
