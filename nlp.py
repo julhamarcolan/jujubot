3ed91b4edb6eb53faaf80585d230cf4afdcd096c
julha Marcolan <47335560+julhamarcolan@users.noreply.github.com>
2024-03-05 21:18:14 -0300

    Update nlp.py
    ADD: documentation

--- nlp.py
+++ nlp.py
@@ -1,15 +1,29 @@
 import google.generativeai as genai
+# Módulo contendo variáveis de ambiente.
 import env 
 
 def ia_chat(user_msg):
-  
-    genai.configure(api_key= env.GOOGLE_API_KEY)
+  """
+    Função para interagir com o modelo de IA para geração de conteúdo.
+
+    Args:
+        user_msg (str): Mensagem do usuário para ser processada pelo modelo.
+
+    Returns:
+        str: Resposta gerada pelo modelo.
+    """
     
+    # Configura a chave da API do Google AI
+    genai.configure(api_key= env.GOOGLE_API_KEY)
+
+    # Lista os modelos disponíveis e verifica se o método de geração de conteúdo é suportado
     for m in genai.list_models():
       if 'generateContent' in m.supported_generation_methods:
         print(m.name)
-    
+
+    # Inicializa o modelo específico ('gemini-pro') para geração de conteúdo
     model = genai.GenerativeModel('gemini-pro')
+    # Gera uma resposta com base na mensagem do usuário
     response = model.generate_content(user_msg)
     msg = response.text
     
